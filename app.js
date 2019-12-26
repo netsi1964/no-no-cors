069af8a7e1195443cb197c7334d523b406981283
Sten Hougaard <netsi1964@gmail.com>
2019-12-26 11:58:18 +0100

    Added express which serves as a proxy

---
+++ app.js
@@ -0,0 +1,31 @@
+const request = require("request");
+const express = require("express");
+const app = express();
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  const host = req.get("host");
+  if (host.includes(`localhost:${port}`) || host.includes("cdpn.io")) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+  }
+
+  next();
+});
+
+app.get("/", (req, res) => {
+  let { term = "Simon Garfunkel" } = req.query;
+  const url = `https://itunes.apple.com/search?term=${term}`;
+  console.log(url);
+  request(url, (error, response, body) => {
+    res.json(response);
+  });
+});
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+# A simpel proxy
+
+This is an Node/Express based simple proxy. It will take a query for `term` and do a request to `https://itunes.apple.com/search?term=` and respond with the response from Apple, if the requester is from CodePen or localhost.
+It has been created as part of a blog post on dev.to: Small API based web app in 3 minutes.
